69534508bf1f7fe1b9b510bf63000e9b7d759bb6
prateekganguly1998 <gangulyprateek0@gmail.com>
2020-08-10 18:26:51 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { AddAppointmentComponent } from './add-appointment/add-appointment.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "addApp", component: AddAppointmentComponent },
+  { path: "viewApp", component: ViewAppointmentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddAppointmentComponent } from './add-appointment/add-appointment.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddAppointmentComponent,
+    ViewAppointmentComponent
   ],
   imports: [
     BrowserModule,
